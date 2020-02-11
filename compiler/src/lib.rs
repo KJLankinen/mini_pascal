d40129cc57ce59892f497e6a6e983a80797064de
Juhana Lankinen <juhana@kjlankinen.com>
2020-02-11 10:40:54 +0200

    Peek and next implemented differently.

--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -71,8 +71,8 @@ pub struct Scanner<'a> {
     chars: std::iter::Peekable<std::str::CharIndices<'a>>, // Iterator over the source string
     token_map: HashMap<&'static str, TokenType>,           // Map for getting the type of a token
     source_str: &'a str,                                   // The entire source as a string slice
-    pub current_token: Option<TokenData<'a>>,              // The current token
-    pub next_token: Option<TokenData<'a>>,                 // Next incoming token for peeking
+    current_token: Option<TokenData<'a>>,                  // The current token
+    next_token: Option<TokenData<'a>>,                     // Next incoming token for peeking
 }
 
 impl<'a> Scanner<'a> {
@@ -119,21 +119,31 @@ impl<'a> Scanner<'a> {
         }
     }
 
-    // Update the current and next tokens
     // Called by parser
-    pub fn step(&mut self) -> Option<&TokenData<'a>> {
-        if self.current_token.is_none() {
+    pub fn next(&mut self) -> Option<&TokenData<'a>> {
+        if self.next_token.is_none() {
             self.current_token = self.get_token().take();
             self.next_token = self.get_token().take();
         } else {
             self.current_token = self.next_token.take();
-            self.next_token = self.get_token();
+            self.next_token = self.get_token().take();
         }
 
         self.current_token.as_ref()
     }
 
-    // Get the next token(s) to member variables
+    pub fn peek(&mut self) -> Option<&TokenData<'a>> {
+        if self.next_token.is_none() {
+            self.next_token = self.get_token().take();
+        }
+
+        self.next_token.as_ref()
+    }
+
+    pub fn current(&mut self) -> Option<&TokenData<'a>> {
+        self.current_token.as_ref()
+    }
+
     fn get_token(&mut self) -> Option<TokenData<'a>> {
         let mut skip_until_newline = false;
         let mut num_nested_comments: i32 = 0;
@@ -295,85 +305,90 @@ impl<'a> Parser<'a> {
     fn process_program(&mut self) {
         self.process_statement_list();
         assert!(
-            TokenType::EndOfProgram == self.scanner.step().unwrap().token_type,
+            TokenType::EndOfProgram == self.scanner.next().unwrap().token_type,
             "{:?}",
-            self.scanner.current_token.unwrap()
+            self.scanner.current().expect("Current token is None.")
         );
     }
 
     fn process_statement_list(&mut self) {
-        self.process_statement();
-        self.process_end_of_statement();
-
+        let token = self.scanner.peek().expect("Next token is None.");
         // Stop if we've reached end of program or the end of 'for' block
-        let token = self.scanner.next_token.unwrap();
         if TokenType::EndOfProgram != token.token_type && TokenType::KeywordEnd != token.token_type
         {
+            self.process_statement();
             self.process_statement_list();
         }
     }
 
     fn process_statement(&mut self) {
-        let token = self.scanner.step().unwrap();
+        self.process_statement_prefix();
+        self.process_end_of_statement();
+    }
+
+    fn process_statement_prefix(&mut self) {
+        let token = self.scanner.next().unwrap();
         match token.token_type {
             TokenType::KeywordVar => {
                 assert!(
-                    TokenType::Identifier == self.scanner.step().unwrap().token_type,
+                    TokenType::Identifier == self.scanner.next().unwrap().token_type,
                     "{:?}",
-                    self.scanner.current_token.unwrap()
+                    self.scanner.current().expect("Current token is None.")
                 );
                 assert!(
-                    TokenType::TypeSeparator == self.scanner.step().unwrap().token_type,
+                    TokenType::TypeSeparator == self.scanner.next().unwrap().token_type,
                     "{:?}",
-                    self.scanner.current_token.unwrap()
+                    self.scanner.current().expect("Current token is None.")
                 );
                 self.process_type();
 
-                if TokenType::Assignment == self.scanner.next_token.unwrap().token_type {
-                    self.scanner.step();
+                if TokenType::Assignment
+                    == self.scanner.peek().expect("Next token is None").token_type
+                {
+                    self.scanner.next();
                     self.process_expression();
                 }
             }
             TokenType::KeywordFor => {
                 assert!(
-                    TokenType::Identifier == self.scanner.step().unwrap().token_type,
+                    TokenType::Identifier == self.scanner.next().unwrap().token_type,
                     "{:?}",
-                    self.scanner.current_token.unwrap()
+                    self.scanner.current().expect("Current token is None.")
                 );
                 assert!(
-                    TokenType::KeywordIn == self.scanner.step().unwrap().token_type,
+                    TokenType::KeywordIn == self.scanner.next().unwrap().token_type,
                     "{:?}",
-                    self.scanner.current_token.unwrap()
-                );
-                self.process_expression();
-                assert!(
-                    TokenType::Range == self.scanner.step().unwrap().token_type,
-                    "{:?}",
-                    self.scanner.current_token.unwrap()
+                    self.scanner.current().expect("Current token is None.")
                 );
                 self.process_expression();
                 assert!(
-                    TokenType::KeywordDo == self.scanner.step().unwrap().token_type,
+                    TokenType::Range == self.scanner.next().unwrap().token_type,
                     "{:?}",
-                    self.scanner.current_token.unwrap()
+                    self.scanner.current().expect("Current token is None.")
+                );
+                self.process_expression();
+                assert!(
+                    TokenType::KeywordDo == self.scanner.next().unwrap().token_type,
+                    "{:?}",
+                    self.scanner.current().expect("Current token is None.")
                 );
                 self.process_statement_list();
                 assert!(
-                    TokenType::KeywordEnd == self.scanner.step().unwrap().token_type,
+                    TokenType::KeywordEnd == self.scanner.next().unwrap().token_type,
                     "{:?}",
-                    self.scanner.current_token.unwrap()
+                    self.scanner.current().expect("Current token is None.")
                 );
                 assert!(
-                    TokenType::KeywordFor == self.scanner.step().unwrap().token_type,
+                    TokenType::KeywordFor == self.scanner.next().unwrap().token_type,
                     "{:?}",
-                    self.scanner.current_token.unwrap()
+                    self.scanner.current().expect("Current token is None.")
                 );
             }
             TokenType::KeywordRead => {
                 assert!(
-                    TokenType::Identifier == self.scanner.step().unwrap().token_type,
+                    TokenType::Identifier == self.scanner.next().unwrap().token_type,
                     "{:?}",
-                    self.scanner.current_token.unwrap()
+                    self.scanner.current().expect("Current token is None.")
                 );
             }
             TokenType::KeywordPrint => {
@@ -381,58 +396,62 @@ impl<'a> Parser<'a> {
             }
             TokenType::KeywordAssert => {
                 assert!(
-                    TokenType::LParen == self.scanner.step().unwrap().token_type,
+                    TokenType::LParen == self.scanner.next().unwrap().token_type,
                     "{:?}",
-                    self.scanner.current_token.unwrap()
+                    self.scanner.current().expect("Current token is None.")
                 );
                 self.process_expression();
                 assert!(
-                    TokenType::RParen == self.scanner.step().unwrap().token_type,
+                    TokenType::RParen == self.scanner.next().unwrap().token_type,
                     "{:?}",
-                    self.scanner.current_token.unwrap()
+                    self.scanner.current().expect("Current token is None.")
                 );
             }
             TokenType::Identifier => {
                 assert!(
-                    TokenType::Assignment == self.scanner.step().unwrap().token_type,
+                    TokenType::Assignment == self.scanner.next().unwrap().token_type,
                     "{:?}",
-                    self.scanner.current_token.unwrap()
+                    self.scanner.current().expect("Current token is None.")
                 );
                 self.process_expression();
             }
-            _ => assert!(false, "{:?}", self.scanner.current_token.unwrap()),
+            _ => assert!(
+                false,
+                "{:?}",
+                self.scanner.current().expect("Current token is None.")
+            ),
         }
     }
 
     fn process_end_of_statement(&mut self) {
         assert!(
-            TokenType::EndOfStatement == self.scanner.step().unwrap().token_type,
+            TokenType::EndOfStatement == self.scanner.next().unwrap().token_type,
             "{:?}",
-            self.scanner.current_token.unwrap()
+            self.scanner.current().expect("Current token is None.")
         );
     }
 
     fn process_type(&mut self) {
-        let token = self.scanner.step().unwrap();
+        let token = self.scanner.next().unwrap();
         assert!(
             TokenType::TypeInt == token.token_type
                 || TokenType::TypeBool == token.token_type
                 || TokenType::TypeString == token.token_type,
             "{:?}",
-            self.scanner.current_token.unwrap()
+            self.scanner.current().expect("Current token is None.")
         );
     }
 
     fn process_expression(&mut self) {
         // Starts with unary op
-        if TokenType::OperatorNot == self.scanner.next_token.unwrap().token_type {
-            self.scanner.step().unwrap();
+        if TokenType::OperatorNot == self.scanner.peek().expect("Next token is None").token_type {
+            self.scanner.next().unwrap();
             self.process_operand();
         } else {
             // Either just a single operand or operand operator operand
             self.process_operand();
             let mut is_operator = true;
-            match self.scanner.next_token.unwrap().token_type {
+            match self.scanner.peek().expect("Next token is None").token_type {
                 TokenType::OperatorPlus => {}
                 TokenType::OperatorMinus => {}
                 TokenType::OperatorMultiply => {}
@@ -444,20 +463,20 @@ impl<'a> Parser<'a> {
             }
 
             if is_operator {
-                self.scanner.step();
+                self.scanner.next();
                 self.process_operand();
             }
         }
     }
 
     fn process_operand(&mut self) {
-        let token = self.scanner.step().unwrap();
+        let token = self.scanner.next().unwrap();
         if TokenType::LParen == token.token_type {
             self.process_expression();
             assert!(
-                TokenType::RParen == self.scanner.step().unwrap().token_type,
+                TokenType::RParen == self.scanner.next().unwrap().token_type,
                 "{:?}",
-                self.scanner.current_token.unwrap()
+                self.scanner.current().expect("Current token is None.")
             );
         } else {
             assert!(
@@ -466,7 +485,7 @@ impl<'a> Parser<'a> {
                     || TokenType::LiteralBool == token.token_type
                     || TokenType::Identifier == token.token_type,
                 "{:?}",
-                self.scanner.current_token.unwrap()
+                self.scanner.current().expect("Current token is None.")
             );
         }
     }
