ab62afa2f031d841d9a63f33bccbc31323e48b7f
Juhana Lankinen <juhana@kjlankinen.com>
2020-02-23 15:02:25 +0200

    Started figuring out different enums for operands and expressions. Does not compile due to some borrow error,

--- compiler/src/parser.rs
+++ compiler/src/parser.rs
@@ -6,6 +6,19 @@ pub struct Parser<'a> {
     scanner: Scanner<'a>,
 }
 
+enum Expression<'a> {
+    Expr(&'a str),
+    Error,
+}
+
+enum Operand<'a> {
+    Expr(Expression<'a>),
+    Int(&'a str),
+    String(&'a str),
+    Identifier(&'a str),
+    Error,
+}
+
 // A recursive descent parser
 impl<'a> Parser<'a> {
     pub fn new(source_str: &'a str) -> Self {
@@ -46,6 +59,8 @@ impl<'a> Parser<'a> {
     }
 
     fn process_statement_list(&mut self) {
+        // Stop recursion if the next token is end of program
+        // or 'end' keyword that ends the for loop.
         match self.scanner.peek().unwrap().token_type {
             TokenType::EndOfProgram | TokenType::KeywordEnd => {}
             _ => {
@@ -57,12 +72,13 @@ impl<'a> Parser<'a> {
 
     fn process_statement(&mut self) {
         self.process_statement_prefix();
-        self.process_end_of_statement();
+        self.match_token(TokenType::EndOfStatement);
     }
 
     fn process_statement_prefix(&mut self) {
         match self.scanner.peek().unwrap().token_type {
             TokenType::KeywordVar => {
+                // Declaration with an optional assignment
                 self.match_token(TokenType::KeywordVar);
                 self.match_token(TokenType::Identifier);
                 self.match_token(TokenType::TypeSeparator);
@@ -75,6 +91,7 @@ impl<'a> Parser<'a> {
                 }
             }
             TokenType::KeywordFor => {
+                // For loop
                 self.match_token(TokenType::KeywordFor);
                 self.match_token(TokenType::Identifier);
                 self.match_token(TokenType::KeywordIn);
@@ -87,20 +104,24 @@ impl<'a> Parser<'a> {
                 self.match_token(TokenType::KeywordFor);
             }
             TokenType::KeywordRead => {
+                // Read statement
                 self.match_token(TokenType::KeywordRead);
                 self.match_token(TokenType::Identifier);
             }
             TokenType::KeywordPrint => {
+                // Print statement
                 self.match_token(TokenType::KeywordPrint);
                 self.process_expression();
             }
             TokenType::KeywordAssert => {
+                // Assertion statement
                 self.match_token(TokenType::KeywordAssert);
                 self.match_token(TokenType::LParen);
                 self.process_expression();
                 self.match_token(TokenType::RParen);
             }
             TokenType::Identifier => {
+                // Assignment to a variable
                 self.match_token(TokenType::Identifier);
                 self.match_token(TokenType::Assignment);
                 self.process_expression();
@@ -111,11 +132,7 @@ impl<'a> Parser<'a> {
         }
     }
 
-    fn process_end_of_statement(&mut self) {
-        self.match_token(TokenType::EndOfStatement);
-    }
-
-    fn process_expression(&mut self) {
+    fn process_expression(&mut self) -> Expression {
         // Starts with unary op
         if TokenType::OperatorNot == self.scanner.peek().unwrap().token_type {
             self.match_token(TokenType::OperatorNot);
@@ -137,23 +154,33 @@ impl<'a> Parser<'a> {
                 _ => {}
             }
         }
+
+        Expression::Error
     }
 
-    fn process_operand(&mut self) {
+    fn process_operand(&mut self) -> Operand {
+        let mut operand = Operand::Error;
         match self.scanner.peek().unwrap().token_type {
-            TokenType::LParen => {
-                self.match_token(TokenType::LParen);
-                self.process_expression();
+            token_type @ TokenType::LParen => {
+                self.match_token(token_type);
+                let expr = self.process_expression();
+                operand = Operand::Expr(expr);
                 self.match_token(TokenType::RParen);
             }
-            token_type @ TokenType::LiteralInt
-            | token_type @ TokenType::LiteralString
-            | token_type @ TokenType::Identifier => {
-                self.match_token(token_type);
+            token_type @ TokenType::LiteralInt => {
+                operand = Operand::Int(self.match_token(token_type).value);
+            }
+            token_type @ TokenType::LiteralString => {
+                operand = Operand::String(self.match_token(token_type).value);
+            }
+            token_type @ TokenType::Identifier => {
+                operand = Operand::Identifier(self.match_token(token_type).value);
             }
             _ => {
                 println!("Missing operand!");
             }
         }
+
+        operand
     }
 }
